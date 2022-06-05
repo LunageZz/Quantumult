9245f51d19404e3f3dbb822e3439abe00afc2644
Lunage <46839077+LunageZz@users.noreply.github.com>
2022-06-05 08:56:54 +0800

    Update wechat.js

--- wechat.js
+++ wechat.js
@@ -1,11 +1,2 @@
 [rewrite_local]
-^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js
-
-[mitm]
-hostname = mp.weixin.qq.com
-
-var obj = JSON.parse($response.body);
-obj.advertisement_num = 0;
-obj.advertisement_info = [];
-delete obj.appid;
-$done({body: JSON.stringify(obj)}); 
+^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad url script-response-body https://raw.githubusercontent.com/LunageZz/QuantumultX/main/wechat.js
